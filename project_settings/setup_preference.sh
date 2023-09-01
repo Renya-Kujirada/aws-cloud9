137829d746d4ae414de50cb5e3234043db1ae289
renya-cpu <renya@example.com>
2023-09-01 13:36:01 +0000

    create cron file and shell to exec auto-shutdown

--- project_settings/setup_preference.sh
+++ project_settings/setup_preference.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # setting for autoshutdown time
+cp c9-automatic-shutdown /etc/cron.d/
+cp stop-if-inactive.sh ~/.c9/
 sudo sh -c 'echo "SHUTDOWN_TIME=30" >> /home/ubuntu/.c9/autoshutdown-configuration'
 
 # setting for python formatter
