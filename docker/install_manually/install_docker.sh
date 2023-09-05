99051250de0c516ce818e20d586e929f532d8f49
renya <rsmilek5492@gmail.com>
2023-09-06 00:25:37 +0900

    create shell to setup gpu docker env manually

---
+++ docker/install_manually/install_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# see https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
